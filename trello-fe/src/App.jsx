57de0bf3b3d7f19897945baef29375fc7a5fa692
Manh Pham <manhpham90vn@icloud.com>
2023-12-26 11:30:22 +0700

    use box component

--- trello-fe/src/App.jsx
+++ trello-fe/src/App.jsx
@@ -1,6 +1,7 @@
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import { Box } from '@mui/material'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -32,9 +33,9 @@ const SelectMode = () => {
           </div>
         </MenuItem>
         <MenuItem value='dark'>
-          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
+          <Box sx={{ display:'flex', alignItems: 'center', gap: 1 }}>
             <DarkModeOutlinedIcon fontSize='small' /> Dark
-          </div>
+          </Box>
         </MenuItem>
         <MenuItem value='system'>
           <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
