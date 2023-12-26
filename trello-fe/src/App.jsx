56b4f4df0112e50e232514668fe2250e8d508c72
Manh Pham <manhpham90vn@icloud.com>
2023-12-26 11:27:12 +0700

    add selectmode component

--- trello-fe/src/App.jsx
+++ trello-fe/src/App.jsx
@@ -1,27 +1,55 @@
-import { Typography } from '@mui/material'
-import Button from '@mui/material/Button/'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
 import {
   useColorScheme
 } from '@mui/material/styles'
 
-const ModeToggle = () => {
+const SelectMode = () => {
   const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="select-dark-light-mode-input">Mode</InputLabel>
+      <Select
+        labelId="select-dark-light-mode-input"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize='small' /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
+            <DarkModeOutlinedIcon fontSize='small' /> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value='system'>
+          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
 const App = () => {
   return (
     <>
-      <ModeToggle />
-      <Typography variant='h1' color='text.primary'>Hello World</Typography>
+      <SelectMode />
     </>
   )
 }
