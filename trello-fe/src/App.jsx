eefe902d4f72c401626092160fbe6218c7471eda
Manh Pham <manhpham90vn@icloud.com>
2023-12-26 17:23:52 +0700

    refactor component

--- trello-fe/src/App.jsx
+++ trello-fe/src/App.jsx
@@ -1,57 +1,11 @@
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import { Box } from '@mui/material'
-import FormControl from '@mui/material/FormControl'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import Select from '@mui/material/Select'
-import {
-  useColorScheme
-} from '@mui/material/styles'
-
-const SelectMode = () => {
-  const { mode, setMode } = useColorScheme()
-
-  const handleChange = (event) => {
-    setMode(event.target.value)
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="select-dark-light-mode-input">Mode</InputLabel>
-      <Select
-        labelId="select-dark-light-mode-input"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value='light'>
-          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
-            <LightModeIcon fontSize='small' /> Light
-          </div>
-        </MenuItem>
-        <MenuItem value='dark'>
-          <Box sx={{ display:'flex', alignItems: 'center', gap: 1 }}>
-            <DarkModeOutlinedIcon fontSize='small' /> Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value='system'>
-          <div style={{ display:'flex', alignItems: 'center', gap: '8px' }}>
-            <SettingsBrightnessIcon fontSize='small' /> System
-          </div>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
+import { Container } from '@mui/material'
+import SelectMode from './SelectMode'
 
 const App = () => {
   return (
-    <>
+    <Container sx={{ height:'100vh', backgroundColor: 'primary.main' }}>
       <SelectMode />
-    </>
+    </Container>
   )
 }
 
--- trello-fe/src/main.jsx
+++ trello-fe/src/main.jsx
@@ -3,7 +3,7 @@ import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/s
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import theme from './theme.js'
+import theme from './Theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
