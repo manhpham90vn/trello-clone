e39bb93e3481a59e20ca08a086fe529c9a218320
Manh Pham <manhpv151090@gmail.com>
2023-12-29 02:25:46 +0000

    Update Docker volume mapping and remove unnecessary commands

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,6 +6,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 exec zsh
-
-yarn install
-yarn dev
